6181596010d8a2e2b7b2b0462a8e815dc4d7a8df
Jonathan Tower <joe.tourangeau@gmail.com>
2017-11-01 22:14:30 -0400

    Refactor dao file

--- src/app/routes/entity.ts
+++ src/app/routes/entity.ts
@@ -2,14 +2,16 @@
  * Use Router object from express
  */
 import * as express from 'express'
-import * as entity from '../dao/entity'
+import { EntityDAO } from '../dao/entity'
 export const router = express.Router()
+
+const entityDAO = new EntityDAO()
 
 /**
  * Define EndPoint routes
  */
-router.get('/entity/get/:id', entity.get)
-router.get('/entity/list', entity.list)
-router.post('/entity/post', entity.post)
-router.patch('/entity/update/:id', entity.update)
-router.delete('/entity/delete/:id', entity.remove)
+router.get('/entity/get/:id', entityDAO.get)
+router.get('/entity/list', entityDAO.list)
+router.post('/entity/post', entityDAO.post)
+router.patch('/entity/update/:id', entityDAO.update)
+router.delete('/entity/delete/:id', entityDAO.remove)
