4158862580450951961f0bfbd002cbe21246021b
Jonathan Tower <joe.tourangeau@gmail.com>
2017-11-12 13:27:37 -0500

    fixes typescript error for not found module

--- __test__/entity.test.ts
+++ __test__/entity.test.ts
@@ -1,4 +1,13 @@
+import { Entity } from '../src/app/models/entity'
+import { EntityDAO } from '../src/app/dao/entity'
 
-// tslint:disable-next-line:no-empty
-test('First test with bookshelf', () => {
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+function sum (a: number, b: number) {
+  return a + b
+}
+it('works with async/await', async () => {
+    //
 })
