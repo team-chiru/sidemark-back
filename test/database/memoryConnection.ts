48b7a28d0c141698da0adbd55e892fc491255901
Jonathan Tower <joe.tourangeau@gmail.com>
2018-03-08 12:46:36 -0500

    Add memoryConnection file

---
+++ test/database/memoryConnection.ts
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+import { Sequelize } from 'sequelize-typescript'
+import * as dotenv from 'dotenv'
+
+// Load environment variables
+dotenv.config()
+
+/**
+ * Get .env value to put in the connection.
+ */
+const database = process.env.SQLITE_DB as string
+const storage = process.env.SQLITE_PATH as string
+
+/**
+ * Initialise SQLITE database connection.
+ */
+export const memoryConnection = new Sequelize({
+  database: database,
+  dialect: 'sqlite',
+  username: 'username',
+  password: 'password',
+  storage:  ':memory:',
+  logging: true
+})
